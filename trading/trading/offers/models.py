e4736e07cf48a56e91785fbaa03bf3c9ca15ed5d
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-05-24 16:54:21 +0300

    added suitable_offers m2m for sale_offer model

--- trading/trading/offers/models.py
+++ trading/trading/offers/models.py
@@ -11,21 +11,6 @@ STATUSES = (
         ('closed', "closed"),
     )
 
-class SaleOffer(models.Model):
-    inventory_item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE)    
-    quantity = models.PositiveIntegerField()
-    price = models.PositiveIntegerField()
-    status = models.CharField(max_length=7, choices=STATUSES)
-
-
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
-
-    def __str__(self):
-        return f'Sale offer {self.inventory_item.currency} {self.quantity} {self.price}'
-
 
 class PurchaseOffer(models.Model):
     currency = models.ForeignKey(Currency, on_delete=models.CASCADE)
@@ -40,3 +25,23 @@ class PurchaseOffer(models.Model):
 
     def __str__(self):
         return f'Purchase offer {self.currency} {self.quantity} {self.price}'
+
+
+class SaleOffer(models.Model):
+    inventory_item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE)
+    suitable_offers = models.ForeignKey(PurchaseOffer, on_delete=models.CASCADE, blank=True)
+    quantity = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+    status = models.CharField(max_length=7, choices=STATUSES)
+
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'Sale offer {self.inventory_item.currency} {self.quantity} {self.price}'
+
+
+
