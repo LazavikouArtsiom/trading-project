74ed24fcb3f6fbe85756dfdfaf90debb4d920f31
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-05-25 12:58:19 +0300

    Trade pre save works correctrly, but need some more additional validation

--- trading/trading/trades/signals.py
+++ trading/trading/trades/signals.py
@@ -1,8 +1,29 @@
-def perform_trade_presave(sender, instance, **kwargs):
-    from models import Trade
-    # if not instance.created:
-    #     Trade.objects.create(user=instance)
-    #     instance.created = True
-    #     instance.save()
+from django.db import transaction
 
-    pass
+from .services import (calculate_transaction_quantity,
+                       calcuclate_money,
+                       close_offers,
+                       offers_trade,
+                       )
+
+
+def perform_trade_presave(sender, instance, **kwargs):
+    from .models import Trade
+
+    sale_offer = instance.sale_offer
+    purchase_offer = instance.purchase_offer
+    
+    sale_quantity_before_trade = sale_offer.quantity
+    purchase_quantity_before_trade = purchase_offer.quantity
+           
+    with transaction.atomic():
+        offers_trade(sale_offer, purchase_offer)
+
+    sale_quantity_after_trade = sale_offer.quantity
+    purchase_quantity_after_trade = purchase_offer.quantity
+
+    instance.purchase_quantity_before_trade = purchase_quantity_before_trade
+    instance.purchase_quantity_after_trade = purchase_quantity_after_trade
+    instance.sale_quantity_before_trade = sale_quantity_before_trade
+    instance.sale_quantity_after_trade = sale_quantity_after_trade
+
