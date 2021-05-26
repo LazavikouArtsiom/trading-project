9f25d84839e346509bc578e65399dc1c7e4c3f38
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-05-26 21:57:31 +0300

    Update urls.py

--- trading/trading/items/urls.py
+++ trading/trading/items/urls.py
@@ -1,15 +1,11 @@
-from re import I
-from django.contrib import admin
 from django.urls import path
-from django.conf import settings
 from rest_framework import routers
 
-from .views import (CurrencyListAPIView,
-                    CurrencyRetrieveAPIView,
-                    )
+from .viewsets import CurrencyReadOnlyViewSet
 
+router = routers.SimpleRouter()
+
+router.register('currencies/')
 
 urlpatterns = [
-    path('', CurrencyListAPIView.as_view()),
-    path('<int:id>', CurrencyRetrieveAPIView.as_view()),
-]
+] + router.urls
