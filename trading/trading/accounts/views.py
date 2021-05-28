245ba3df71ebf50b22eb8691f1d51b428da43f1d
LazavikouArtsiom <59964790+LazavikouArtsiom@users.noreply.github.com>
2021-05-28 12:01:20 +0300

    select logic moved to selector

--- trading/trading/accounts/views.py
+++ trading/trading/accounts/views.py
@@ -3,13 +3,14 @@ from rest_framework.response import Response
 
 from trading.accounts.models import Account
 from trading.accounts.serializers import AccountSerializer
+from trading.accounts import selectors
 
 
 class AccountRetrieveUpdateAPIView(RetrieveUpdateAPIView):
     serializer_class = AccountSerializer
     
     def get_queryset(self):
-        return Account.objects.get(user=self.request.user)
+        return selectors.get_user_account(self)
 
     def get(self, request):
         queryset = self.get_queryset()
